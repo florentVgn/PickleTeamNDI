1b15068a7a721402e823ab78c44d72e03ccee9c2
chanthaj <jeremie82@live.fr>
2017-12-07 23:09:20 +0100

    Ajout models

---
+++ application/models/Trajet_model.php
@@ -0,0 +1,32 @@
+<?php
+
+class Trajet_model extends CI_Model{
+
+
+    protected $table = "adresse";
+
+    public function create($trajetInfos)
+    {
+        return $this->db->set($trajetInfos)
+            ->insert($this->table);
+    }
+
+    public function get($id)
+    {
+        return $this->db->get_where($this->table,array('id'=>$id));
+    }
+
+    public function update($champs,$donnee)
+    {
+        return $this->db->set($donnee)
+            ->where($champs)
+            ->update($this->table);
+    }
+
+    public function delete($id)
+    {
+        return $this->db->where($id)
+            ->delete($this->table);
+    }
+
+}
