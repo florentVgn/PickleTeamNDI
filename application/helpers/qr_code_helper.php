f8a0f2be19facc09569b930cc4fb50603b7c8de6
chanthaj <jeremie82@live.fr>
2017-12-08 03:55:07 +0100

    Ajout qr code helper & lib

--- application/helpers/qr_code_helper.php
+++ application/helpers/qr_code_helper.php
@@ -4,7 +4,13 @@ if (!defined('BASEPATH'))
 
 if (!function_exists('generate_qrc')) {
     function generate_qrc($url, $fileName){
-        // TODO:
+        $CI =& get_instance();
+        $CI->load->library('ciqrcode');
+        $params['data'] = $url;
+        $params['level'] = 'H';
+        $params['size'] = 10;
+        $params['savename'] = FCPATH.'/assets/uploads/qr_codes/'.$fileName.'.png';
+        $CI->ciqrcode->generate($params);
     }
 }
 
