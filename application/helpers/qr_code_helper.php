ec7a69f5d5af95c96a082142dcaf56930362a909
Mathieu Aubé <mathieu.aube@outlook.com>
2017-12-08 03:35:10 +0100

    Création du helper permettant la génération des QR codes

---
+++ application/helpers/qr_code_helper.php
@@ -0,0 +1,12 @@
+<?php
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+if (!function_exists('generate_qrc')) {
+    function generate_qrc($url, $fileName){
+        // TODO:
+    }
+}
+
+/* End of file tools_helper.php */
+/* Location: ./application/helpers/tools_helper.php */
