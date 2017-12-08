1252d3b956146770bb01edfd2ae0b8f58805d517
chanthaj <jeremie82@live.fr>
2017-12-08 01:03:39 +0100

    Table sql modifiés (plus de mdp, plus d'etablissement)

--- create.sql
+++ create.sql
@@ -10,13 +10,13 @@ CREATE TABLE Adresse(
 CREATE TABLE Utilisateur(
   id_utilisateur INT(11) PRIMARY KEY AUTO_INCREMENT,
   mail VARCHAR(255) UNIQUE,
-  mdp BINARY(20),
+  /*mdp BINARY(20),*/
   nom VARCHAR(255),
   prenom VARCHAR(255),
-  id_adresse int(11),
+  tel int(10),
   FOREIGN KEY(id_adresse) REFERENCES Adresse(id_adresse)
 );
-
+/*
 CREATE TABLE Etablissement(
   id_etablissement int(11) PRIMARY KEY,
   id_proprio int(11),
@@ -27,16 +27,17 @@ CREATE TABLE Etablissement(
   FOREIGN KEY (id_proprio) REFERENCES Utilisateur(id_utilisateur),
   FOREIGN KEY (id_adresse) REFERENCES Adresse(id_adresse)
 );
+*/
 
 CREATE TABLE Evenement(
   id_evenement INT(11) PRIMARY KEY,
-  id_etablissement INT(11),
+  /*id_etablissement INT(11),*/
   id_organisateur INT(11),
   id_adresse INT(11) DEFAULT NULL,
   nom VARCHAR(255),
   date_deb DATE,
   date_fin DATE,
-  FOREIGN KEY (id_etablissement) REFERENCES Etablissement(id_etablissement),
+  /*FOREIGN KEY (id_etablissement) REFERENCES Etablissement(id_etablissement),*/
   FOREIGN KEY (id_organisateur) REFERENCES Utilisateur(id_utilisateur),
   FOREIGN KEY (id_adresse) REFERENCES Adresse(id_adresse)
 );
@@ -46,6 +47,9 @@ CREATE TABLE Trajet(
   id_sam INT(11),
   h_depart INT(4) CHECK (h_depart BETWEEN 0000 AND 2400),
   nb_place INT(2),
+  code_modif INT(4),
+  id_event INT(11),
+  FOREIGN KEY (id_event) REFERENCES Evenement(id_evenement),
   FOREIGN KEY (id_sam) REFERENCES Utilisateur(id_utilisateur)
 );
 
--- jettests.sql
+++ jettests.sql
@@ -1,9 +1,8 @@
 INSERT INTO Adresse(ville,adrese,cp,pays) VALUES ('Valence','16 rue Truc',26000,'France');
-INSERT INTO Utilisateur(mail,mdp,nom,prenom,id_adresse) VALUES ('chantharath.jeremie@gmail.com','test','chantharath','jeremie',0);
-INSERT INTO Utilisateur(mail,mdp,nom,prenom,id_adresse) VALUES ('truc.jeremie@gmail.com','test','truc','jeremie',0);
-INSERT INTO Utilisateur(mail,mdp,nom,prenom,id_adresse) VALUES ('chose.jeremie@gmail.com','test','chose','jeremie',0);
-INSERT INTO Etablissement(id_proprio,id_adresse,nom,mail_contact,tel_contact) VALUES (1,0,'truc','chantharath.jeremie@gmail.com', 0600000000);
-INSERT INTO Evenement(id_etablissement,id_organisateur,id_adresse,nom,date_deb,date_fin) VALUES (0,1,0,'chose',now(),NULL) ;
+INSERT INTO Utilisateur(mail,nom,prenom,tel) VALUES ('chantharath.jeremie@gmail.com','chantharath','jeremie',0600000000);
+INSERT INTO Utilisateur(mail,mdp,nom,prenom,tel) VALUES ('truc.jeremie@gmail.com','truc','jeremie',0600000000);
+INSERT INTO Utilisateur(mail,mdp,nom,prenom,tel) VALUES ('chose.jeremie@gmail.com','chose','jeremie',0600000000);
+INSERT INTO Evenement(id_organisateur,id_adresse,nom,date_deb,date_fin) VALUES (1,0,'chose',now(),NULL) ;
 INSERT INTO Trajet(id_sam,h_depart,nb_place) VALUES (1,10,2);
 INSERT INTO InscritTrajet (id_inscrit,id_trajet) VALUES (1,0);
 
